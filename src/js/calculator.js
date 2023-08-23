a6c4409b5c34bcbcb351a9343d01539bcbc21781
DimaDevelopment <dima.development@fakel.com.ua>
2023-08-23 17:10:28 +0300

    fixed calctulator v2

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -6,6 +6,7 @@ export const createCalculator = async () => {
   const currencies = await fetchCurrencies();
 
   const fromSelect = document.querySelector('[data-select="from"]');
+  const toSelect = document.querySelector('[data-select="to"]');
 
   const reverseButton = document.querySelector('[data-el="reverse-button"]');
 
@@ -23,18 +24,20 @@ export const createCalculator = async () => {
 
   fromInput.addEventListener("input", (event) => {
     const value = event.target.value || 0;
-    const currency = fromSelect.innerText;
+    const fromCurrency = fromSelect.innerText;
+    const toCurrency = toSelect.innerText;
 
-    const result = convertToCZK(value, currency, currencies);
+    const result = convertCurrency(value, fromCurrency, toCurrency, currencies);
 
     toInput.value = result;
   });
 
   toInput.addEventListener("input", (event) => {
     const value = event.target.value || 0;
-    const currency = fromSelect.innerText;
+    const fromCurrency = toSelect.innerText;
+    const toCurrency = fromSelect.innerText;
 
-    const result = convertFromCZK(value, currency, currencies);
+    const result = convertCurrency(value, fromCurrency, toCurrency, currencies);
 
     fromInput.value = result;
   });
@@ -42,41 +45,96 @@ export const createCalculator = async () => {
   const fromSelectDropdown = selectEls[0].querySelector(
     '[data-el="select-dropdown"]'
   );
+  const toSelectDropdown = selectEls[1].querySelector(
+    '[data-el="select-dropdown"]'
+  );
 
   Array.from(fromSelectDropdown.children).forEach((child) => {
     child.addEventListener("click", () => {
       const value = fromInput.value || 0;
-      const currency = child.dataset.value;
+      const fromCurrency = child.dataset.value;
+      const toCurrency = toSelect.innerText;
 
-      const result = convertToCZK(value, currency, currencies);
+      const result = convertCurrency(
+        value,
+        fromCurrency,
+        toCurrency,
+        currencies
+      );
 
       toInput.value = result;
     });
   });
+
+  Array.from(toSelectDropdown.children).forEach((child) => {
+    child.addEventListener("click", () => {
+      const value = toInput.value || 0;
+      const fromCurrency = fromSelect.innerText;
+      const toCurrency = child.dataset.value;
+
+      const result = convertCurrency(
+        value,
+        fromCurrency,
+        toCurrency,
+        currencies
+      );
+
+      fromInput.value = result;
+    });
+  });
+
+  reverseButton.addEventListener("click", () => {
+    const toCurrency = toSelect.innerText;
+    const fromCurrency = fromSelect.innerText;
+
+    const fromFlagEl = selectEls[0].querySelector('[data-el="select-icon"]');
+    const toFlagEl = selectEls[1].querySelector('[data-el="select-icon"]');
+
+    const fromFlag = fromFlagEl.src;
+    const toFlag = toFlagEl.src;
+
+    const fromAmount = fromInput.value || 0;
+    const toAmount = toInput.value || 0;
+
+    fromSelect.innerText = toCurrency;
+    toSelect.innerText = fromCurrency;
+
+    fromInput.value = toAmount;
+    toInput.value = fromAmount;
+
+    fromFlagEl.src = toFlag;
+    toFlagEl.src = fromFlag;
+  });
 };
 
-const convertToCZK = (amount, fromCurrency, currencies) => {
+const convertCurrency = (amount, fromCurrency, toCurrency, currencies) => {
   const from = currencies.find((currency) => currency[0] === fromCurrency);
+  const to = currencies.find((currency) => currency[0] === toCurrency);
 
-  if (!from) {
+  if (fromCurrency === "CZK") {
+    const toRate = parseFloat(to[3]);
+
+    const convertedAmount = amount / toRate;
+
+    return Math.ceil(convertedAmount);
+  }
+
+  if (toCurrency === "CZK") {
+    const fromRate = parseFloat(from[2]);
+
+    const convertedAmount = amount * fromRate;
+
+    return Math.ceil(convertedAmount);
+  }
+
+  if (!from || !to) {
     return;
   }
 
   const fromRate = parseFloat(from[3]);
-  const convertedAmount = amount * fromRate;
+  const toRate = parseFloat(to[2]);
 
-  return Math.ceil(convertedAmount);
-};
-
-const convertFromCZK = (amount, fromCurrency, currencies) => {
-  const from = currencies.find((currency) => currency[0] === fromCurrency);
-
-  if (!from) {
-    return;
-  }
-
-  const fromRate = parseFloat(from[3]);
-  const convertedAmount = amount / fromRate;
+  const convertedAmount = (amount * fromRate) / toRate;
 
   return Math.ceil(convertedAmount);
 };
